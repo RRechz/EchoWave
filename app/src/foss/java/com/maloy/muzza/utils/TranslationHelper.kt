0c7b35d8ddc12417c567280b00918b5f60fcf8e3
Mustafa Burak Özcan <m06.burakozcan@proton.me>
2025-01-30 22:15:30 +0300

    Delete app directory

--- app/src/foss/java/com/maloy/muzza/utils/TranslationHelper.kt
+++
@@ -1,8 +0,0 @@
-package com.maloy.muzza.utils
-
-import com.maloy.muzza.db.entities.LyricsEntity
-
-object TranslationHelper {
-    suspend fun translate(lyrics: LyricsEntity): LyricsEntity = lyrics
-    suspend fun clearModels() {}
-}
